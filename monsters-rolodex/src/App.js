6111f66d5803e0d6e10217babfeb952c5eaff40d
Waleed Tariq <76990392+hadAbyte@users.noreply.github.com>
2022-12-22 05:02:59 +0500

    Update App.js

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: {
+        firstName: "Waleed",
+        lastName: "Tariq",
+      },
+      company: "Code Creatives",
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hi, {this.state.name.firstName} {this.state.name.lastName} and I
+            work at {this.state.company}
+          </p>
+          <button
+            onClick={() => {
+              this.setState(
+                () => {
+                  return {
+                    name: { firstName: "Rehman", lastName: "Ahmed" },
+                  };
+                },
+                () => {
+                  console.log(this.state);
+                }
+              );
+            }}
+          >
+            Change name
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
