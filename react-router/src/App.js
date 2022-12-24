46dd687e3d95dcdc5a7e80cc0524ee20703f79a0
Waleed Tariq <76990392+waleedtariq109@users.noreply.github.com>
2022-12-25 03:44:54 +0500

    adding links

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
+import NavBar from "./components/Navbar";
 import "./App.css";
 
 const App = () => {
   return (
     <>
       <div className="App">
+        <NavBar />
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
