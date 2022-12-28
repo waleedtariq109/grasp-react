8daee59717007bb0be7507d56b4a114b0d1bd909
Waleed Tariq <76990392+waleedtariq109@users.noreply.github.com>
2022-12-29 03:07:27 +0500

    Update Home.js

--- react-router/src/components/Home.js
+++ react-router/src/components/Home.js
@@ -1,7 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
+  const navigate = useNavigate();
   return (
     <>
       <h1>This is Home Page</h1>
+      <button onClick={() => navigate("/about-us")}>About</button>
+      <button onClick={() => navigate("/filter?age=10&city=multan")}>
+        User
+      </button>
     </>
   );
 };
